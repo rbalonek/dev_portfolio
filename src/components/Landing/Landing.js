43051e5ab46181569369033adc3597dbcdc6d675
Robert Balonek <robertbalonek@gmail.com>
2021-08-06 10:18:50 -0700

    Awesome-reveal

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Landing.css";
+import { Fade } from "react-awesome-reveal";
 
 export default function Landing() {
   return (
     <div className="landing__container">
       <h1>Robert Balonek</h1>
       <h3>Software Engineer</h3>
-      <div className="arrow">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+      <Fade delay={1000} duration={2000} triggerOnce={true}>
+        <div className="arrow">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </Fade>
     </div>
   );
 }
