2e896883ac80e4a62aa23811d56330754a8f1096
Robert Balonek <robertbalonek@gmail.com>
2021-08-06 10:16:50 -0700

    Landing setup

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <div className="landing__container">
+      <h1>Robert Balonek</h1>
+      <h3>Software Engineer</h3>
+      <div className="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
