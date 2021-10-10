550deb2b6bf2c9a107b47925e98710075ee245a1
Robert Balonek <robertbalonek@gmail.com>
2021-10-10 09:36:17 -0700

    Animations

--- src/components/ProjectAppStore/ProjectAppStore.js
+++ src/components/ProjectAppStore/ProjectAppStore.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./ProjectAppStore.css";
+import { Roll, AttentionSeeker } from "react-awesome-reveal";
 
 export default function ProjectAppStore(props) {
   return (
     <div className="app-store-icon-split">
-      <a target="_blank" rel="noopener noreferrer" href={props.website}>
-        <img className="project__app-store" alt="icon" src={props.appStore} />
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href={props.website}>
-        <img className="project__icon" alt="appStore" src={props.icon} />
-      </a>
+      <AttentionSeeker effect="tada" delay="200">
+        <a target="_blank" rel="noopener noreferrer" href={props.website}>
+          <img className="project__app-store" alt="icon" src={props.appStore} />
+        </a>
+      </AttentionSeeker>
+
+      <Roll direction="left" delay="100">
+        <AttentionSeeker effect="tada" delay="200">
+          <a target="_blank" rel="noopener noreferrer" href={props.website}>
+            <img className="project__icon" alt="appStore" src={props.icon} />
+          </a>
+        </AttentionSeeker>
+      </Roll>
     </div>
   );
 }
